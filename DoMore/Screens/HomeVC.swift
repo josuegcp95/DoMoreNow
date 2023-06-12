5e1693e6e2c102048ef03590e465e81345a52de1
Josue <josuegcp95@gmail.com>
2023-06-11 21:45:27 -0400

    General bug fixes and improvements

--- DoMore/Screens/HomeVC.swift
+++ DoMore/Screens/HomeVC.swift
@@ -12,7 +12,7 @@ class HomeVC: UIViewController {
     let tableView = UITableView()
     var localLibrary: [Action] = []
     let notificationCenter = UNUserNotificationCenter.current()
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
